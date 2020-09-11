d43e8767feb59eab442b698006a4f276ede1c927
waleedahm3d <waleedahm5d@gmail.com>
2020-09-11 16:29:57 +0500

    Delete user & all associated articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,7 +40,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    redirect_to articles_path
+    redirect_to user_path(current_user)
   end
 
   # All the methods below this keyword are only accessible in this controller
